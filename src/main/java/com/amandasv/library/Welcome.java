854f741b2001ba4d6a36826c58c6dde0ba068fd5
Amanda <amandasv.pm@gmail.com>
2017-07-15 12:55:29 -0300

    create welcome class

---
+++ src/main/java/com/amandasv/library/Welcome.java
@@ -0,0 +1,10 @@
+package com.amandasv.library;
+
+
+public class Welcome {
+
+    public String showMessage() {
+        String message = "Hello! Welcome, the library is open now";
+        return message;
+    }
+}
