9c6e6f1b2bf536ba66b91974cbb39daa39fc709c
Amanda <amandasv.pm@gmail.com>
2017-07-17 23:39:05 -0300

    update main and welcome message

--- src/main/java/com/amandasv/library/Main.java
+++ src/main/java/com/amandasv/library/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Welcome welcome = new Welcome();
-        System.out.println(welcome.showMessage());
+        Printer printer = new Printer();
+
+        printer.print(welcome.showMessage());
     }
 }
