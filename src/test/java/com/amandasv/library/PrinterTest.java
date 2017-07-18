e8cdc0d38459f507804203e673fed1551149fe85
Amanda <amandasv.pm@gmail.com>
2017-07-17 23:39:33 -0300

    start Printer test

---
+++ src/test/java/com/amandasv/library/PrinterTest.java
@@ -0,0 +1,14 @@
+package com.amandasv.library;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PrinterTest {
+
+//    @Test
+//    public void correctlyShowString() {
+//        Printer printer = new Printer();
+//        Assert.assertEquals("Welcome Message", printer.print("Welcome Message"));
+//    }
+}
